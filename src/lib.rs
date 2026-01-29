ec4d4a78d6cb55872db4e0d34f06a1bad451cd80
flopetautschnig (floscodes) <mail@floscodes.net>
2026-01-29 12:27:05 +0100

    fix: hard reset to make it compile on mings32

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod verushash;
 
-pub fn verus_hash(data: &[u8]) -> [u8; 32] {
+pub fn verus_hash_v1(data: &[u8]) -> [u8; 32] {
     let mut out = [0u8; 32];
     unsafe {
         verushash::verus_v1_hash(out.as_mut_ptr(), data.as_ptr(), data.len());
@@ -38,25 +38,23 @@ mod tests {
     use hex_literal::hex;
 
     #[test]
-    fn test_hashes() {
+    fn test_verus_hash_v1() {
         let data = b"hello world";
-        let h1 = verus_hash(data);
-        let h2 = verus_hash_v2(data);
-        assert_ne!(h1, h2); // v1 und v2 should be different
+        let _h1 = verus_hash_v1(data);
     }
 
     #[test]
     fn test_verus_hash_v2_2_standard() {
         let version: u32 = 4;
-        let mut prev_block =
+        let prev_block =
             hex!("0000000000000000000000000000000000000000000000000000000000000000");
-        let mut merkle_root =
+        let merkle_root =
             hex!("1111111111111111111111111111111111111111111111111111111111111111");
-        let mut sapling_root =
+        let sapling_root =
             hex!("2222222222222222222222222222222222222222222222222222222222222222");
         let time: u32 = 1600000000;
         let bits: u32 = 0x1d00ffff; // nBits als u32
-        let mut nonce = hex!("3333333333333333333333333333333333333333333333333333333333333333");
+        let nonce = hex!("3333333333333333333333333333333333333333333333333333333333333333");
 
         let mut header = Vec::with_capacity(140);
         header.extend_from_slice(&version.to_le_bytes());
